26dea6d7f18830310a52deb483964cee6f2656db
cjohst <connor.johst@gmail.com>
2025-02-17 14:48:15 -0800

    removed redundant datasource initialization.

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -24,12 +24,3 @@ export const AppDataSource = new DataSource({
   migrations: [],
   subscribers: [],
 });
-
-// Initialize the DataSource
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
