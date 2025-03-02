b96a1e174d48d93c878b9f58ebcbeb6536bfea04
Maddy Paulson <madeline1paulson@gmail.com>
2025-03-02 15:04:33 -0800

    Fix url.

--- frontend/app/src/main/java/com/example/gameon/api/interfaces/GroupsApi.kt
+++ frontend/app/src/main/java/com/example/gameon/api/interfaces/GroupsApi.kt
@@ -1,6 +1,7 @@
 package com.example.gameon.api.interfaces
 
 import com.example.gameon.classes.GroupMember
+import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,7 +15,7 @@ interface GroupsApi {
     suspend fun getGroupUrl(@Path("id") groupId: Int): Response<GroupUrlResponse>
 
     data class GroupUrlResponse(
+        @SerializedName("groupurl")
         val groupUrl: String
     )
-
 }
