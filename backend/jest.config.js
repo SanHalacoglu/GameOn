44562f70a05687da67af0938b87be2ed67864935
cjohst <connor.johst@gmail.com>
2025-03-06 20:12:09 -0800

    added config file

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'src/**/*.{ts,js}',
+      '!src/**/*.d.ts',
+    ],
+    coverageDirectory: 'coverage',
+  };
